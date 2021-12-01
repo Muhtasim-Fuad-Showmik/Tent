f3dc3987fd4b34c994355fa2dfe5364833f41cce
Muhtasim Fuad Showmik <49075537+Muhtasim-Fuad-Showmik@users.noreply.github.com>
2021-12-01 11:38:04 +0600

    Finished Implementing Basic CRUD functionalities
    Added CRUD functionalities to the software for the database, "campgrounds". Campgrounds can now be created, read, updated and deleted through the existing User Interface.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
     
 // Connecting mongoose database
@@ -19,16 +20,62 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+//All "app.use" content runs for every single request and response cycle.
+
+// Used for parsing request bodies
+app.use(express.urlencoded({extended: true}));
+// Used for overriding methods and therefore generating several other method requests
+app.use(methodOverride('_method'));
+
 // Rendering default page
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-// Link for creating a campground by default from within this file.
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping'});
-    await camp.save();
-    res.send(camp);
+// Link for displaying all campgrounds within the database.
+app.get('/campgrounds', async (req, res) => {
+    // Fetching all campgrounds.
+    const campgrounds = await Campground.find({});
+    // Sending all campgrounds to the ejs file for rendering.
+    res.render('campgrounds/index', { campgrounds })
+})
+
+// Link for a form to add new campgrounds to the application
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+// Link for uploading the data collected from the for adding a new campground.
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`campgrounds/${campground._id}`);
+})
+
+// Link for redirecting to the details of a campground
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground });
+})
+
+// Link for accessing the form for updating existing campground data.
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground });
+})
+
+// Link for submission of updated data for the campground.
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+// Link for deleting a campground
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 })
 
 // Listeningi to the server using port 3000

98bda9996fc1f6afc6e9407c581caecec605e2d6
Muhtasim Fuad Showmik <49075537+Muhtasim-Fuad-Showmik@users.noreply.github.com>
2021-12-13 01:01:20 +0600

    Added Multer and tried it out
    Added the Multer library into the project and tried their image uploading functionalities.

--- routes/campgroundRoutes.js
+++ routes/campgroundRoutes.js
@@ -5,11 +5,16 @@ const { campgroundSchema } = require('../schemas.js');
 const campgroundController = require('../controllers/campgroundController');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 
 router.route('/')
     .get(catchAsync(campgroundController.index)) // Link for displaying all campgrounds within the database.
-    .post(isLoggedIn, validateCampground, catchAsync(campgroundController.createCampground)); // Link for uploading the data collected from the form adding a new campground.
+    //.post(isLoggedIn, validateCampground, catchAsync(campgroundController.createCampground)); // Link for uploading the data collected from the form adding a new campground.
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+    });
 
 // Link for a form to add new campgrounds to the application
 router.get('/new', isLoggedIn, campgroundController.renderNewForm);

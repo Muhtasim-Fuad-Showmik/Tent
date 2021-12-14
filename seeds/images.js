b70235d3ed880f0843ced7db7afe8e9756bfedb1
Muhtasim Fuad Showmik <49075537+Muhtasim-Fuad-Showmik@users.noreply.github.com>
2021-12-15 03:38:06 +0600

    Modified Seeds to allow for generation of better data
    Images are now randomly added to each campground during the seeding procedure. The number of images on each campground is also randomized. The descriptions are also random but realistic now.

---
+++ seeds/images.js
@@ -0,0 +1,110 @@
+module.exports = [
+    {
+        filename: "Tent/papczdmvlc5qk6tvmwek",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516375/Tent/papczdmvlc5qk6tvmwek.jpg"
+    },
+    {
+        filename: "Tent/he9nvcnfxb4qjsrqztxb",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516375/Tent/he9nvcnfxb4qjsrqztxb.jpg"
+    },
+    {
+        filename: "Tent/jgsg2vauezgpxixw2plm",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516374/Tent/jgsg2vauezgpxixw2plm.jpg"
+    },
+    {
+        filename: "Tent/dcjk9n9nyxycysxmgcv1",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516373/Tent/dcjk9n9nyxycysxmgcv1.jpg"
+    },
+    {
+        filename: "Tent/xeo8wk0outybpynxac3s",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516372/Tent/xeo8wk0outybpynxac3s.jpg"
+    },
+    {
+        filename: "Tent/c3lezoyhbqqrfooocpgd",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516371/Tent/c3lezoyhbqqrfooocpgd.jpg"
+    },
+    {
+        filename: "Tent/dgblbrg764f9ku3soh4d",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516370/Tent/dgblbrg764f9ku3soh4d.jpg"
+    },    
+    {
+        filename: "Tent/x9mhysrzfxpuou9sv61b",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516369/Tent/x9mhysrzfxpuou9sv61b.jpg"
+    },
+    {
+        filename: "Tent/jbk619phk6uf5zozo3yf",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516367/Tent/jbk619phk6uf5zozo3yf.jpg"
+    },
+    {
+        filename: "Tent/nghufacwd18ndcysn11g",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516366/Tent/nghufacwd18ndcysn11g.jpg"
+    },
+    {
+        filename: "Tent/fcqtmke9nxchuou6tsmd",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516365/Tent/fcqtmke9nxchuou6tsmd.jpg"
+    },
+    {
+        filename: "Tent/kfgodu5ifzsjvdnnyp3z",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516364/Tent/kfgodu5ifzsjvdnnyp3z.jpg"
+    },
+    {
+        filename: "Tent/stkushdugx5onqlhklbr",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516363/Tent/stkushdugx5onqlhklbr.jpg"
+    },
+    {
+        filename: "Tent/zxwfovsl4dn3xjmdomir",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516362/Tent/zxwfovsl4dn3xjmdomir.jpg"
+    },
+    {
+        filename: "Tent/dhpjq3bmmpxo1qbixamr",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516361/Tent/dhpjq3bmmpxo1qbixamr.jpg"
+    },
+    {
+        filename: "Tent/gmfqvd4myldiwsfp1fa8",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516361/Tent/gmfqvd4myldiwsfp1fa8.jpg"
+    },
+    {
+        filename: "Tent/vlclwvdb7xkpugbyzsyt",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516360/Tent/vlclwvdb7xkpugbyzsyt.jpg"
+    },
+    {
+        filename: "Tent/luygb3un0xulpo2knw08",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516359/Tent/luygb3un0xulpo2knw08.jpg"
+    },
+    {
+        filename: "Tent/ppmud07s9hjox9mqi4zb",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516358/Tent/ppmud07s9hjox9mqi4zb.jpg"
+    },
+    {
+        filename: "Tent/ubz6uxemzol83e4betqs",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516357/Tent/ubz6uxemzol83e4betqs.jpg"
+    },
+    {
+        filename: "Tent/axdxpzvcykpsni1ivww8",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516357/Tent/axdxpzvcykpsni1ivww8.jpg"
+    },
+    {
+        filename: "Tent/qdw7ufrubanahhrdpmac",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516357/Tent/qdw7ufrubanahhrdpmac.jpg"
+    },
+    {
+        filename: "Tent/kmi07oiowsh4duzi8zpt",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516357/Tent/kmi07oiowsh4duzi8zpt.jpg"
+    },
+    {
+        filename: "Tent/x64vblbpwii1tpk629t1",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516356/Tent/x64vblbpwii1tpk629t1.jpg"
+    },
+    {
+        filename: "Tent/fjrn5ckcfrc8iimtxvcs",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516355/Tent/fjrn5ckcfrc8iimtxvcs.jpg"
+    },
+    {
+        filename: "Tent/k7jd8ip3mq7aisoucxki",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516354/Tent/k7jd8ip3mq7aisoucxki.jpg"
+    },
+    {
+        filename: "Tent/vkkxiow0oybsweujv1ga",
+        url: "https://res.cloudinary.com/fuad-dev/image/upload/v1639516353/Tent/vkkxiow0oybsweujv1ga.jpg"
+    },
+]
--- seeds/index.js
+++ seeds/index.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
+const descriptions = require('./descriptions');
+const images = require('./images');
 const {places, descriptors} = require('./seedHelpers');
 const Campground = require('../models/campground');
 const review = require('../models/review');
+const user = require('../models/user');
     
 // Connecting mongoose database
 mongoose.connect('mongodb://localhost:27017/tent');
@@ -21,36 +24,41 @@ const seedDB = async () => {
     // Delete all previous entries on first execution of this file.
     await Campground.deleteMany({});
     await review.deleteMany({});
+    await user.deleteMany({});
+
+    // Create the default user
+    const defaultUser = new user({
+        email : "fuad@gmail.com", 
+        username : "Fuad", 
+        salt : "5aeac9f65b1c37616089a51977247c2e9c6fe7bfa8ab7ac64f5f83a784f56ae1", 
+        hash : "2db18cbd7c44938fd43b0b793a47eb1945357f135325cc72e0169eab5ba0cd10e08a8a9868af2e1ef2c18b635877124d15b1fa8381afcf594d8cce8a198c20431cd7299ea9f4d4dd8412262dd406082f9ec517893551cbe32d367009c5011c93b4f4e32150df6009c89dca1be27ed4429c34ec8e2cfebe8eb1db8734dafd4f67d413929a70e1a8b8351098b4fe6cdc6eebcdfafe58a252b95b7bc1806f58b967efcea8b64e6e8f829fc4638358a4c3828d81342cca951b17e003cb9b5e107e9a9b91ec9ed84d5410ba804c24434cbdc46885ae3b98216af37125af12c0b6b4794fe9873def39880e1b9bddb2e394d2d4641827707116b7e97198baa2502c779911d46b7e3ffb92c37bfba689f21f01dbdbd49f5d8ca49c70a6cd6cd86b9b7e31af98883e0906eea7f8d943f8f1b889d1e7e0c4752a64c56e031783f23b85d9301fa8c47cc326862bf01270533d7cf71e128796974c5c09f471247943e462acbb1b6a0c32bab59c294b2ff905481c0a3667d17974c7f62ba8a5d646e11f8977baea5847a5defd57147cb5f1c3fc4b83213d3890ea17a61c9380aa40c826826c95b168ac97d68df98b704df693c4ac83b677ade6db93d0ccec14454b515c296415c447c0dd7961e4ed1a7715e43a5da0577d6c2fbd28684b078b50f130176f8c8d52555b7c10795f2707da0d95572a1b8faeb5235ec4974cf82ad5fedf17c8aa17",
+    });
+
+    const savedUser = await defaultUser.save();
 
     // Create 50 random records with locations and titles of the campgrounds
     for(let i=0; i < 300; i++){
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
+
+        const randomImages = [];
+        const numberOfImages = 1 + random1000 % 5;
+        for(let i=0; i < numberOfImages; i++){
+            randomImages[i] = images[(random1000 + i) % images.length];
+        }
         const camp = new Campground({
-            author: '61b8b5dfbe22e705ee4ee707',
+            author: savedUser._id,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, sint voluptatum similique omnis quibusdam, odit porro reprehenderit repudiandae placeat molestias vero a itaque. Totam corrupti ipsum nam quaerat quo doloremque.',
+            description: `${descriptions[random1000 % descriptions.length]}`,
             price,
             geometry: { 
                 type : "Point", 
                 coordinates : [ cities[random1000].longitude, cities[random1000].latitude ] 
             },
-            images: [
-                { 
-                    "url" : "https://res.cloudinary.com/fuad-dev/image/upload/v1639382122/Tent/prp8uy1f6riak7hdnw8t.jpg",
-                    "filename" : "Tent/prp8uy1f6riak7hdnw8t"
-                }, 
-                { 
-                    "url" : "https://res.cloudinary.com/fuad-dev/image/upload/v1639382123/Tent/vidchxzeojxhnclc7lhn.jpg",
-                    "filename" : "Tent/vidchxzeojxhnclc7lhn"
-                },
-                { 
-                    "url" : "https://res.cloudinary.com/fuad-dev/image/upload/v1639382124/Tent/ouc8pbuwpic7m1eyqpev.jpg",
-                    "filename" : "Tent/ouc8pbuwpic7m1eyqpev"
-                }
-            ]
-        })
+            images: randomImages
+        });
+
         await camp.save();
     }
 }
